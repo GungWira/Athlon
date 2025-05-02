9b78313dad356a1ba6a38cc633ee11ebe85912c0
GungWira <wiradarma002@gmail.com>
2025-05-02 14:20:44 +0800

    setup react router dom

--- src/Athlon_frontend/src/App.jsx
+++ src/Athlon_frontend/src/App.jsx
@@ -1,34 +1,7 @@
-import { useState } from "react";
-import { Athlon_backend } from "declarations/Athlon_backend";
+import AppRoutes from "./routes";
 
 function App() {
-  const [greeting, setGreeting] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    Athlon_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
-  return (
-    <main>
-      <p className="text-blue-600">halo</p>
-      <h1 className="text-3xl text-emerald-400">haloo</h1>
-      <p></p>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
