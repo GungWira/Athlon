8e653ed2055b0825f48ce96845ecd4d23d31510d
GungWira <wiradarma002@gmail.com>
2025-05-03 12:34:21 +0800

    Adding basic logic for creating profile

--- src/Athlon_frontend/src/routes/index.jsx
+++ src/Athlon_frontend/src/routes/index.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import ProtectedAuthRoute from "./protectedAuthRoute";
+import CreateProfile from "../pages/CreateProfile";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/createprofile" element={<CreateProfile />} />
+
+      <Route element={<ProtectedAuthRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
     </Routes>
   );
 }
