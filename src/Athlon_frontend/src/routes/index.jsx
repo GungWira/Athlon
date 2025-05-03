68a12600518a841cee6a7bd38d6a4f0b06859d04
moshcat <mauricecathoverile@gmail.com>
2025-05-03 12:54:06 +0800

    change layout route

--- src/Athlon_frontend/src/routes/index.jsx
+++ src/Athlon_frontend/src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import AuthLayout from "../layouts/AuthLayout";
+import MainLayout from "../layouts/MainLayout";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<AuthLayout/>}>
+      <Route path="/" element={<MainLayout/>}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
       </Route>
